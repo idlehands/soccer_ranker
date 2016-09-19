61d9076cf12333dc8cec3b91be5192e10bec9655
Jeffrey Matthias <jeffrey@parkifi.com>
2016-09-18 21:10:44 -0600

    Complete collector tests and logic.

--- lib/collector.ex
+++ lib/collector.ex
@@ -2,8 +2,12 @@ defmodule SoccerRanker.Collector do
   use GenServer
   require Logger
 
+  defmodule Results do
+    defstruct team_scores: %{}, teams_with_errors: []
+  end
+
   def start_link() do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+    GenServer.start_link(__MODULE__, %Results{}, name: __MODULE__)
   end
 
   def init(args) do
@@ -24,7 +28,7 @@ defmodule SoccerRanker.Collector do
   end
   def handle_call(:report, _from, state) do
     results = ordered_results(state)
-    {:reply, results, state}
+    {:reply, %{ordered_teams: results, teams_with_errors: state.teams_with_errors}, state}
   end
 
   def handle_cast(message, state) do
@@ -37,28 +41,31 @@ defmodule SoccerRanker.Collector do
     {:noreply, state}
   end
 
-  defp update_points({team1data,team2data}, []) do
-    update_points({team1data, team2data}, %{})
-  end
-  defp update_points({team1data, team2data}, state) when is_map(state) do
+  defp update_points({team1data, team2data}, state) do
     state
     |> aggregate_points(team1data)
     |> aggregate_points(team2data)
   end
 
+  defp aggregate_points(state, {team_name, :error}) do
+    %{state | teams_with_errors: Enum.uniq([team_name | state.teams_with_errors])}
+  end
   defp aggregate_points(state, {team_name, new_points}) do
-    existing_points = state[team_name] || 0
+    existing_points = state.team_scores[team_name] || 0
     new_total = existing_points + new_points
-    Map.put(state, team_name, new_total)
+    %{state | team_scores: Map.put(state.team_scores, team_name, new_total)}
   end
 
   defp ordered_results(state) do
-    Map.to_list(state)
+    Map.to_list(state.team_scores)
     |> Enum.sort(&score_sort(&1, &2))
   end
 
-  def score_sort({_team1name, team1points}, {_team2name, team2points}) when team1points != team2points do
+  defp score_sort({_team1name, team1points}, {_team2name, team2points}) when team1points != team2points do
     team1points > team2points
+  end
+  defp score_sort({team1name, team1points}, {team2name, team2points}) when team1points == team2points do
+    team1name < team2name
   end
 end
 
--- test/collector_test.exs
+++ test/collector_test.exs
@@ -9,6 +9,28 @@ defmodule SoccerRankerTest.CollectorTest do
 
     results = Collector.report
 
-    assert results == [{"Snakes", 4}, {"Badgers", 1}]
+    assert results.ordered_teams == [{"Snakes", 4}, {"Badgers", 1}]
+  end
+
+  test "it alphabetizes teams with matching scores" do
+    Collector.start_link
+    Collector.add_points({{"Snakes", 1},{"Badgers", 1}})
+    Collector.add_points({{"Anacondas", 1},{"Zebras FC", 1}})
+
+    results = Collector.report
+
+    assert results.ordered_teams == [{"Anacondas", 1},{"Badgers", 1}, {"Snakes", 1}, {"Zebras FC", 1}]
+  end
+
+  test "it returns teams names that have errors" do
+    Collector.start_link
+    Collector.add_points({{"Snakes", 1}, {"Elephants", 1}})
+    Collector.add_points({{"Snakes", :error}, {"Elephants", :error}})
+    # add a second set to make sure that we don't repeat the names in the results
+    Collector.add_points({{"Snakes", :error}, {"Elephants", :error}})
+    results = Collector.report
+
+    assert results.ordered_teams == [{"Elephants", 1}, {"Snakes", 1}]
+    assert Enum.sort(results.teams_with_errors) == ["Elephants", "Snakes"]
   end
 end
